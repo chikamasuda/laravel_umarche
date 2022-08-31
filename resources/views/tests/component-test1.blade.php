2f7ed707611d5b52482e646e52570536186bb01d
chikamasuda <c.m.062820@gmail.com>
2022-08-31 19:28:45 +0900

    セクション3終了

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,8 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー１</x-slot>
   コンポーネントテスト１
+
+  <x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.card>
+  <x-tests.card title="タイトル2" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
